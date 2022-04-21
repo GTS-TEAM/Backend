e3a7d47d922cc750f5d13a9090a437df1d05b759
Minh Nguyen <3langn@gmail.com>
2022-04-22 02:43:38 +0700

    123

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+import (
+	"fmt"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+func main() {
+
+
+	dsn := "host=localhost user=postgres password=postgres dbname=gorm port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	_, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Hello World!")
+}
