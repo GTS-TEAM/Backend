a759abe4fe7f077698e226c4cd73754c701e84ac
Minh Nguyen <3langn@gmail.com>
2022-04-22 02:44:46 +0700

    123

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 func main() {
 
 
-	dsn := "host=localhost user=postgres password=postgres dbname=gorm port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	dsn := "host=localhost user=postgres password=postgres dbname=next_db port=5432 sslmode=disable TimeZone=Asia/Shanghai"
 	_, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
