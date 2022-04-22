88744cb4d49dfb2ba2cd2134ca135fb1bfa8948e
Minh Nguyen <3langn@gmail.com>
2022-04-22 23:35:37 +0700

    cors

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-contrib/gzip"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	uuid "github.com/satori/go.uuid"
 	"log"
 	"next/controllers"
 	"next/models"
@@ -14,6 +16,32 @@ func TokenAuthMiddleware() gin.HandlerFunc {
 	t := &models.Token{}
 	return func(c *gin.Context) {
 		t.TokenValid(c)
+		c.Next()
+	}
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost")
+		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding, x-access-token")
+		c.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Length")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		if c.Request.Method == "OPTIONS" {
+			fmt.Println("OPTIONS")
+			c.AbortWithStatus(200)
+		} else {
+			c.Next()
+		}
+	}
+}
+
+func RequestIDMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		uu := uuid.NewV4()
+		c.Writer.Header().Set("X-Request-Id", uu.String())
 		c.Next()
 	}
 }
@@ -30,6 +58,9 @@ func main() {
 	}
 
 	r := gin.Default()
+
+	r.Use(CORSMiddleware())
+	r.Use(RequestIDMiddleware())
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
 	r.Use(gzip.Gzip(gzip.DefaultCompression))
